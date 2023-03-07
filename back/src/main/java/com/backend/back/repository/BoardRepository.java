be73b79876ef616ea303227af757a497a311798c
gytjd <hys339631@gmail.com>
2023-03-08 00:25:43 +0900

    BoardRepository 생성

---
+++ back/src/main/java/com/backend/back/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.backend.back.repository;
+
+import com.backend.back.Domain.board.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board,Long> {
+}
