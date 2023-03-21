3eaee26a90eb784c22159d6d52a5d46b1cd0b772
gytjd <hys339631@gmail.com>
2023-03-21 11:04:26 +0900

    api controller 수정

---
+++ back/src/main/java/com/backend/back/api/Controller/boardController.java
@@ -0,0 +1,24 @@
+package com.backend.back.api.Controller;
+
+import com.backend.back.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@CrossOrigin
+public class boardController {
+
+    private final BoardService boardService;
+
+    /**
+     * 게시물 등록
+     */
+
+
+
+
+}
